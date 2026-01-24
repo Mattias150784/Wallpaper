a672c6f4f62d1ca7f144ca1a8aa00c946a28b2d8
MattiasAye <Mattiasayenew@gmail.com>
2026-01-23 22:14:56 -0800

    Added MixinBlock

---
+++ neoforge/src/main/java/net/mattias/wallpaper/neoforge/mixin/MixinBlock.java
@@ -0,0 +1,50 @@
+package net.mattias.wallpaper.neoforge.mixin;
+
+import net.mattias.wallpaper.neoforge.core.data.ForgeWallpaperData;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
+
+@Mixin(Block.class)
+public abstract class MixinBlock {
+    @Inject(method = "shouldRenderFace(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/Direction;Lnet/minecraft/core/BlockPos;)Z", at = @At("HEAD"), cancellable = true)
+    private static void shouldRenderFaceHook(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, Direction direction, BlockPos blockPos2, CallbackInfoReturnable<Boolean> cir) {
+        Minecraft mc = Minecraft.getInstance();
+        Level level = mc.level;
+        if (level == null) return;
+
+        var wallpaperStorage = ForgeWallpaperData.getData(level).storage;
+        if (wallpaperStorage.isEmpty()) return;
+
+        for (Map.Entry<BlockPos, Map<Direction, BlockState>> entry : wallpaperStorage.entrySet()) {
+            Map<Direction, BlockState> wallpapers = entry.getValue();
+
+            BlockPos pos = entry.getKey();
+
+            if (!blockPos.equals(pos)) continue;
+
+            if (wallpapers == null || wallpapers.isEmpty()) continue;
+
+            for (Map.Entry<Direction, BlockState> wpEntry : wallpapers.entrySet()) {
+                Direction face = wpEntry.getKey();
+                BlockState wpState = wpEntry.getValue();
+
+                if (wpState == null) continue;
+
+                if (direction == face) {
+                    cir.setReturnValue(false);
+                }
+            }
+        }
+    }
+}
